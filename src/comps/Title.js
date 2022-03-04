4524949395b62f9fca3b45ea5978b3a836f80c00
sonika panigrahi <sonika.panigrahi@gmail.com>
2022-03-04 12:58:29 +0530

    Changed Title

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from "react";
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1>FireStock</h1>
       <h2>@herthoughts_in_sky</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
